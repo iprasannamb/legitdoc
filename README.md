f483a09834e563c36bfb808a809a98e1e9d63d75
Prasannakumar Badiger <iprasannamb@gmail.com>
2025-09-24 23:23:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Generative AI for Demystifying Legal Documents
+
+##  AI solution that simplifies complex legal documents into clear, accessible guidance, empowering users to make informed decisions.
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Card from "../../component/Card";
+export default function Home() {
+  return (
+    <>
+<div className="h-12 text-2xl font-medium flex justify-center items-center  ">
+<Card/>
+</div> 
+    </>
+  );
+}
