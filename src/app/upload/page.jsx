826beceedb5957d38cc06acca70567c7839745fc
Prasanna <iprasannamb@gmail.com>
2025-12-28 23:06:58 +0530

    feat:Add file  preview functionality

--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -1,19 +1,39 @@
-"use client"
-import { Input } from "@/components/ui/input"
+"use client";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function page() {
-const fileHandle=(e)=>{
-console.log(e.target.files)
-}
+  // const [file, setFile] = useState(null);
+  const [prev, setPrev] = useState(null);
+  const fileHandle = (e) => {
+    const file = e.target.files[0]
+    setPrev(URL.createObjectURL(file));
+  };
   return (
-  <>
+    <>
+      <div className="flex h-52 items-center justify-center">
+        <Input
+          type="file"
+          placeholder="Email"
+          className={"w-50 bg-gray-100"}
+          onChange={fileHandle}
+        />
+      </div>
+      <div
+        id="preview"
+        className="flex items-center justify-center h-96 w-full "
+      >
+        {!prev ? (
+          <p className="text-gray-400 text-sm">No file selected</p>
+        ) : (
+          <img
+            src={prev}
+            alt="Image preview"
+            className="max-h-full max-w-full object-contain rounded"
+          />
+        )}
+      </div>
 
-<div className="flex h-52 items-center justify-center">
-
-  <Input type="file" placeholder="Email" className={'w-50 bg-gray-100'} onChange={fileHandle}/>
-</div>
-
-
-  </>)
-  
+    </>
+  );
 }
